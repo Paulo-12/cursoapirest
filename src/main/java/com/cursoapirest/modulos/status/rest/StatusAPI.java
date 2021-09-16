46db4c217901fb7cca52027886ecdbd387ed70a9
Paulo-12 <ricardocardoso021@bol.com.br>
2021-09-15 21:06:02 -0300

    Primeiro commit

---
+++ src/main/java/com/cursoapirest/modulos/status/rest/StatusAPI.java
@@ -0,0 +1,17 @@
+package com.cursoapirest.modulos.status.rest;
+
+import com.cursoapirest.modulos.status.dto.StatusDTO;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
+import org.springframework.http.ResponseEntity;
+
+public interface StatusAPI {
+  @ApiResponses(
+    {
+      @ApiResponse(code = 200, message = "Ok Api funcionando"),
+      @ApiResponse(code = 404, message = "Não encontrou a Api"),
+      @ApiResponse(code = 401, message = "Não tem permissão"),
+    }
+  )
+  ResponseEntity<StatusDTO> getStatus();
+}
